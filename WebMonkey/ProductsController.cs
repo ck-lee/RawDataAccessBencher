8452edb37d80e0b4f407e424c50b792f6809baa3
CK <chaokoon@gmail.com>
2015-07-02 23:00:49 +1200

    Add web monkey

---
+++ WebMonkey/ProductsController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.OData;
+using EF6.Bencher;
+using EF6.Bencher.EntityClasses;
+
+namespace WebMonkey
+{
+    public class ProductsController : ODataController
+    {
+        AWDataContext db = new AWDataContext();
+
+        // GET api/<controller>
+        [EnableQuery]
+        public IQueryable<Product> Get()
+        {
+            return db.Products;
+        }
+
+    }
+}
---
+++ WebMonkey/Startup.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.OData.Batch;
+using System.Web.OData.Builder;
+using System.Web.OData.Extensions;
+using EF6.Bencher.EntityClasses;
+using Microsoft.OData.Edm;
+using Microsoft.Owin;
+using Microsoft.Owin.Extensions;
+using Owin;
+
+[assembly: OwinStartup(typeof(WebMonkey.Startup))]
+
+namespace WebMonkey
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            var config = new HttpConfiguration();
+            config.MapHttpAttributeRoutes();
+            app.UseWebApi(config);
+
+            ODataModelBuilder builder = new ODataConventionModelBuilder();
+            var products = builder.EntitySet<Product>("Products");
+            products.EntityType.Ignore(p => p.BillOfMaterials);
+            products.EntityType.Ignore(p => p.BillOfMaterials_);
+
+            products.EntityType.Ignore(p => p.ProductCostHistories);
+            products.EntityType.Ignore(p => p.ProductDocuments);
+            products.EntityType.Ignore(p => p.ProductInventories);
+            products.EntityType.Ignore(p => p.ProductListPriceHistories);
+            products.EntityType.Ignore(p => p.ProductModel);
+            products.EntityType.Ignore(p => p.ProductProductPhotos);
+            products.EntityType.Ignore(p => p.ProductReviews);
+            products.EntityType.Ignore(p => p.ProductSubcategory);
+            products.EntityType.Ignore(p => p.ProductVendors);
+            products.EntityType.Ignore(p => p.PurchaseOrderDetails);
+            products.EntityType.Ignore(p => p.ShoppingCartItems);
+            products.EntityType.Ignore(p => p.SpecialOfferProducts);
+            products.EntityType.Ignore(p => p.TransactionHistories);
+            products.EntityType.Ignore(p => p.UnitMeasure);
+            products.EntityType.Ignore(p => p.UnitMeasure_);
+            products.EntityType.Ignore(p => p.WorkOrders);
+
+            config.MapODataServiceRoute(
+                routeName: "ODataRoute",
+                routePrefix: null,
+                model: builder.GetEdmModel());
+            config.EnsureInitialized();
+        }
+
+        
+    }
+}
